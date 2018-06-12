1964fb4aa45d5736c3e84f50a4e311c385a11c19
jiaxin.li <jiaxin.li@qunar.com>
2018-06-12 19:52:12 +0800

    update data

---
+++ server/dbo.js
@@ -0,0 +1,85 @@
+var express = require('express');
+var MongoClient = require('mongodb').MongoClient;
+var router = express.Router();
+var url = 'mongodb://localhost:27017/';
+
+router.get('/select', function(req, res) { // 查询数据
+    MongoClient.connect(url, function(err, db) {
+        if(err) throw err;
+        var dbo = db.db('analysis');
+        dbo.collection('emotion').find().toArray(function(err, resp) {
+            if(err) throw err;
+            res.send(resp);
+            db.close();
+        });
+    });
+});
+
+router.post('/modify', function(req, res) { // 修改标签
+    var result = req.body;
+    MongoClient.connect(url, function(err, db) {
+        if(err) throw err;
+        var dbo = db.db('analysis');
+        var where = {'label.name': result.name},
+            update = {$set: {['data.' + result.index + '.case']: result.value}};
+        dbo.collection('emotion').updateOne(where, update, function(err, resp) {
+            if(err) throw err;
+            res.send(true);
+            db.close();
+        });
+    });
+});
+
+router.post('/delete', function(req, res) { // 删除数据
+    var result = req.body;
+    MongoClient.connect(url, function(err, db) {
+        if(err) throw err;
+        var dbo = db.db('analysis');
+        var where = {'label.name': result.name},
+            update1 = {$set: {['data.' + result.index]: null}},
+            update2 = {$pull: {data: null}}
+        dbo.collection('emotion').updateOne(where, update1, function(err, resp) { // 先将要删除的元素设置为null
+            if (err) throw err;
+            db.close();
+        });
+        dbo.collection('emotion').updateOne(where, update2, function(err, resp) { // 然后将数组中的null删除
+            if (err) throw err;
+            res.send(true);
+            db.close();
+        });
+    });
+});
+
+router.post('/add', function(req, res) { // 添加情绪分析
+    var result = req.body;
+    MongoClient.connect(url, function(err, db) {
+        if(err) throw err;
+        var dbo = db.db('analysis');
+        dbo.collection('emotion').insertOne(result.params, function(err, resp) {
+            if(err) throw err;
+            res.send(true);
+            db.close();
+        });
+    });
+});
+
+router.post('/addTab', function(req, res) { // 添加data数据
+    var result = req.body;
+    dataSource = {
+        areaData: result.areaData,
+        case: result.case
+    };
+    MongoClient.connect(url, function(err, db) {
+        if(err) throw err;
+        var dbo = db.db('analysis');
+        var where = {'label.name': result.name},
+            update = {$push: {data: dataSource}};
+        dbo.collection('emotion').updateOne(where, update, function(err, resp) {
+            if(err) throw err;
+            res.send(true);
+            db.close();
+        });
+    });
+});
+
+module.exports = router;
